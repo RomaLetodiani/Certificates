3b1f9e88b599827092fb953e9c320f6714115622
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-01 10:54:17 +0400

    vercel analytics

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { Analytics } from "@vercel/analytics/react";
 import CertificatesDiv from "./Components/Certificates";
 
 const App = () => {
   return (
     <div className="w-full min-h-screen p-5 min-w-[350px] bg-gradient-to-br from-green-300 to-blue-700">
+      <Analytics />
       <CertificatesDiv />
     </div>
   );
